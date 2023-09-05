0aa78fbe58f98fcc484e5b2a9b2643d427ca97d9
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-05 14:03:05 +0500

    Another Example of Spread Op.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ let a = [1,2 ,3,4,5,6], c = 99;
 let b = [c, ...a], d = [...a,c];
 console.log(`B after Spread [c, ...a] == ${b}`)
 console.log(`D after Spread [...a,c] == ${d}`)
+
+// Another Example
+arr = [1,2,4,6,32]
+let avg = (a,b,c) =>{
+    console.log("A=== ",a)
+    console.log("b=== ",b)
+    console.log("c=== ",c)
+
+    return a+b+c/3;
+}
+
+console.log("avg === ",avg(...arr))
