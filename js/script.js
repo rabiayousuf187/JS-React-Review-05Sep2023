b03c123052aa93faef9eaaa31cfef6aa340f30c4
Syed Ali Naqvi <rabiayousuf187@gmail.com>
2023-09-05 13:55:49 +0500

    Spread Opreator Start

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@
 
 // Spread Opreator
 let a = [1,2 ,3,4,5,6], c = 99;
-
+let b = [c, ...a], d = [...a,c];
+console.log(`B after Spread [c, ...a] == ${b}`)
+console.log(`D after Spread [...a,c] == ${d}`)
