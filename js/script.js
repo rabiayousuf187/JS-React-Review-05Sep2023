946f69678e4520c01ade285c357eee35355fe3ee
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-09-05 14:19:32 +0500

    Async Js

--- js/script.js
+++ js/script.js
@@ -2,29 +2,44 @@
 
 // --------------- Spread Opreator
 console.log("* Spread Opreator Start")
-let a = [1,2 ,3,4,5,6], c = 99;
-let b = [c, ...a], d = [...a,c];
+let a = [1, 2, 3, 4, 5, 6], c = 99;
+let b = [c, ...a], d = [...a, c];
 console.log(`B after Spread [c, ...a] == ${b}`)
 console.log(`D after Spread [...a,c] == ${d}`)
 
 // Another Example
-arr = [1,2,4,6,32]
-let avg = (a,b,c) =>{
-    console.log("A=== ",a)
-    console.log("b=== ",b)
-    console.log("c=== ",c)
+arr = [1, 2, 4, 6, 32]
+let avg = (a, b, c) => {
+    console.log("A=== ", a)
+    console.log("b=== ", b)
+    console.log("c=== ", c)
 
-    return a+b+c/3;
+    return a + b + c / 3;
 }
 
-console.log("avg === ",avg(...arr))
+console.log("avg === ", avg(...arr))
 console.log("Spread Opreator End")
-// --------------- Destructuring
-let num = {ac:3, ad:67}
-console.log("* Destructuring Start")
 
-let {ac,ad} = num;
+// --------------- Destructuring
+console.log("* Destructuring Start")
+let num = { ac: 3, ad: 67 }
+
+let { ac, ad } = num;
 console.log(`ac ===== ${ac}`)
 console.log(`ad ===== ${ad}`)
 
 console.log("Destructuring End")
+
+// --------------- Async Js
+console.log("* Async Js Start")
+
+// 1
+console.log("1. Harry is a good boy!");
+// 2
+setTimeout(() => {
+    console.log("2. Harry is inside setTimeout");
+}, 1000);
+// 3
+console.log("3. Harry is a bad boy!");
+
+console.log("Async Js End")
